b7e2faa22828d97f9483dc9feebbb370253ea5e4
Matthew Parry <matthew@mparry.com.au>
2020-02-27 13:50:21 +1100

    Got postpro() to work.

--- ldisp.m
+++ ldisp.m
@@ -1,6 +1,8 @@
 function ldisp(fid, message)
     % Write to log file.
-	fprintf(fid, [convertStringsToChars(message), '\r\n']);
+    if fid ~= -1
+        fprintf(fid, [convertStringsToChars(message), '\r\n']);
+    end
     
 	% Write to console
 	fprintf([convertStringsToChars(message) '\n']); % To command window.
