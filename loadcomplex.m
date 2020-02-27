2d36015037e9a65dd07d2f0c58709cddececf7c1
Matthew Parry <matthew@mparry.com.au>
2020-02-27 12:30:24 +1100

    load complex numbers from data file

---
+++ loadcomplex.m
@@ -0,0 +1,26 @@
+function out = loadcomplex(fname)
+    out = [];
+    
+    % Open file
+    [fd, errmsg] = fopen(fname, 'r');
+    
+    if fd == -1
+        error( 'loadcomplex: %s: %s\n', fname, errmsg);
+    end
+    
+    cur_line = fgets(fd);
+    while cur_line ~= -1
+        data = cell2mat(textscan(cur_line, '%f'));
+        if ~isempty(data)
+            out = [out; data'];
+        end
+        cur_line = fgets(fd);
+    end
+    
+    %close file
+    close_status = fclose(fd);
+    if close_status == -1
+        warning('loadcomplex: Error while closing file.\n');
+    end
+    
+end
