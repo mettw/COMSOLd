e162c30c8ff84a4869eea6c84913c8ef37eaaa9c
Matthew Parry <matthew@mparry.id.au>
2024-08-21 13:54:21 +1000

    small improvement

--- post_processing/set_COMSOLdStudyConfig_project_example.m
+++ post_processing/set_COMSOLdStudyConfig_project_example.m
@@ -1,13 +1,15 @@
-function [fname, available_projects] = set_COMSOLdStudyConfig_project(project)
+function out = set_COMSOLdStudyConfig_project(project)
 
-    available_projects = ["Giorgio" "DARPA"];
-    
-    switch project
-        case "Giorgio"
-            fname = "C:\Users\Matthew\MATLAB Drive\Giorgio\configure_COMSOLdStudyConfig.m";
-        case "DARPA"
-            fname = "C:\Users\Matthew\MATLAB Drive\DARPA\configure_COMSOLdStudyConfig.m";
-        otherwise
-            error("COMSOLdStudyConfig(): Project not configured.");
+    if isempty(project) % get list of available projects
+        out = ["Giorgio" "DARPA"];
+    else    
+        switch project
+            case "Giorgio"
+                out = "C:\Users\Matthew\MATLAB Drive\Giorgio\configure_COMSOLdStudyConfig.m";
+            case "DARPA"
+                out = "C:\Users\Matthew\MATLAB Drive\DARPA\configure_COMSOLdStudyConfig.m";
+            otherwise
+                error("COMSOLdStudyConfig(): Project not configured.");
+        end
     end
 end
